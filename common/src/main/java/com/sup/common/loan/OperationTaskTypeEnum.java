adf838747685d98d9ab1c60abe8ae47b0edd9df8
ghost <guanfeng2007@qq.com>
2019-09-21 11:03:12 +0800

    update tb_apply_info(add asset_level); add tb_operation_task;

---
+++ common/src/main/java/com/sup/common/loan/OperationTaskTypeEnum.java
@@ -0,0 +1,43 @@
+package com.sup.common.loan;
+
+/**
+ * Project:uniloan
+ * Class:  OperationTaskTypeEnum
+ * <p>
+ * Author: guanfeng
+ * Create: 2019-09-12
+ */
+
+public enum OperationTaskTypeEnum {
+    // 任务类型：0:初审，1:复审，2:终审，3:逾期（未还）
+
+    TASK_FIRST_AUDIT(0, "初审任务"),
+    TASK_SECOND_AUDIT(1, "复审任务"),
+    TASK_FINAL_AUDIT(2, "终审任务"),
+    TASK_OVERDUE(3, "逾期任务");
+
+    private int code;
+    private String codeDesc;
+
+    OperationTaskTypeEnum(int code, String desc) {
+        this.code = code;
+        this.codeDesc = desc;
+    }
+
+    public int getCode() {
+        return this.code;
+    }
+
+    public String getCodeDesc() {
+        return this.codeDesc;
+    }
+
+    public static OperationTaskTypeEnum getStatusByCode(int code) {
+        for (OperationTaskTypeEnum responseCode : OperationTaskTypeEnum.values()) {
+            if (responseCode.getCode() == code) {
+                return responseCode;
+            }
+        }
+        return null;
+    }
+}
