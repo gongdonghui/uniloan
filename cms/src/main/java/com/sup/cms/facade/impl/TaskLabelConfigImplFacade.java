506225c7a132a5f318bde399ce57e11f1c4b75a3
ghost <guanfeng2007@qq.com>
2019-11-01 22:49:58 +0800

    update cms

--- cms/src/main/java/com/sup/cms/facade/impl/TaskLabelConfigImplFacade.java
+++ cms/src/main/java/com/sup/cms/facade/impl/TaskLabelConfigImplFacade.java
@@ -4,12 +4,17 @@ import com.sup.cms.facade.TaskLabelConfigFacade;
 import com.sup.common.param.GetLabelParam;
 import com.sup.common.param.TaskLabelParam;
 import com.sup.common.util.Result;
+import lombok.extern.log4j.Log4j;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * gongshuai
  * <p>
  * 2019/10/14
  */
+
+@RestController
+@Log4j
 public class TaskLabelConfigImplFacade implements TaskLabelConfigFacade {
     @Override
     public Result addLabel(TaskLabelParam param) {
