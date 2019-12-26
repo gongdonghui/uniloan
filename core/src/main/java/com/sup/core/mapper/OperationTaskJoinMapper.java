df66950dc71fd147fa57f96e02c497a113cd050f
ghost <guanfeng2007@qq.com>
2019-12-26 16:13:58 +0800

    fix sql error

--- core/src/main/java/com/sup/core/mapper/OperationTaskJoinMapper.java
+++ core/src/main/java/com/sup/core/mapper/OperationTaskJoinMapper.java
@@ -46,8 +46,8 @@ public interface OperationTaskJoinMapper extends BaseMapper {
             "b.need_total  as  total,   b.act_total as  total_actual" +
             " from tb_operation_task a" +
             " join tb_repay_stat b on a.apply_id=b.apply_id" +
-            " where a.task_type= ${task_type}" +
+            " where a.task_type= ${taskType}" +
             " and create_time< ${end} ")
-    List<CollectionStatBean>  getCollectionStats(Date end, Integer tasktype);
+    List<CollectionStatBean>  getCollectionStats(Date end, Integer taskType);
 
 }
