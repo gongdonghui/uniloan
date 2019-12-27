2b833853b481a07936ebef6b3d8558a051e6d3c0
ghost <guanfeng2007@qq.com>
2019-12-27 15:13:45 +0800

    fix bug for task

--- core/src/main/java/com/sup/core/mapper/OperationTaskJoinMapper.java
+++ core/src/main/java/com/sup/core/mapper/OperationTaskJoinMapper.java
@@ -6,7 +6,6 @@ import com.sup.core.bean.CollectionStatBean;
 import com.sup.core.bean.OperationTaskJoinBean;
 import org.apache.ibatis.annotations.Select;
 
-import java.util.Date;
 import java.util.List;
 
 /**
@@ -25,8 +24,8 @@ public interface OperationTaskJoinMapper extends BaseMapper {
             " from tb_operation_task a" +
             " join tb_apply_info b on a.apply_id=b.id" +
             " where a.task_type= ${taskType}" +
-            "  and a.create_time >=${start} and a.create_time< ${end} ")
-    List<OperationTaskJoinBean> getOperationTaskJoinByTask(Date start, Date  end, Integer taskType );
+            "  and a.create_time >='${start}' and a.create_time< '${end}' ")
+    List<OperationTaskJoinBean> getOperationTaskJoinByTask(String start, String  end, Integer taskType );
 
 
     @Select("select " +
@@ -38,8 +37,8 @@ public interface OperationTaskJoinMapper extends BaseMapper {
             "( select  apply_id, sum(replay_amount)  as repay_sum from   tb_repay_history " +
             " where   repay_time > ${start} and  repay_time < ${end} and repay_status =1 group by apply_id) as b " +
             " on a.apply_id=b.apply_id" +
-            " where  a.create_time >=${start} and a.create_time< ${end} ")
-    List<CollectionRepayBean> getCollectionRepay(Date start, Date  end);
+            " where  a.create_time >='${start}' and a.create_time< '${end}' ")
+    List<CollectionRepayBean> getCollectionRepay(String start, String  end);
 
     @Select("select " +
             "a.apply_id as applyId, a.create_time as create_time, " +
@@ -47,7 +46,7 @@ public interface OperationTaskJoinMapper extends BaseMapper {
             " from tb_operation_task a" +
             " join tb_repay_stat b on a.apply_id=b.apply_id" +
             " where a.task_type= ${taskType}" +
-            " and create_time< ${end} ")
-    List<CollectionStatBean>  getCollectionStats(Date end, Integer taskType);
+            " and create_time< '${end}' ")
+    List<CollectionStatBean>  getCollectionStats(String end, Integer taskType);
 
 }
