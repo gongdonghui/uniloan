1eca0952e58e9d9bea4024296469829d9e5b1731
ghost <guanfeng2007@qq.com>
2019-12-30 18:47:10 +0800

    update cms&core(update bank account info & retry loan)

--- common/src/main/java/com/sup/common/param/OperationReportParam.java
+++ common/src/main/java/com/sup/common/param/OperationReportParam.java
@@ -1,5 +1,6 @@
 package com.sup.common.param;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.util.Date;
@@ -11,8 +12,12 @@ import java.util.Date;
  */
 @Data
 public class OperationReportParam {
+    @JsonFormat(pattern="yyyy-MM-dd")
     private Date start_date;
+
+    @JsonFormat(pattern="yyyy-MM-dd")
     private Date end_date;
+
     private Integer channel_id;
     private Integer type;   //0  渠道日报   2   运营日报   3     审批日报，  4  催收日报
 }
