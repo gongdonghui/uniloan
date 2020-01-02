20375202c7312b7779c7d92d01291cfc4b5214cf
ghost <guanfeng2007@qq.com>
2020-01-02 15:33:54 +0800

    update cms(condition not work for loan query)

--- cms/src/main/java/com/sup/cms/controller/LoanController.java
+++ cms/src/main/java/com/sup/cms/controller/LoanController.java
@@ -1,5 +1,6 @@
 package com.sup.cms.controller;
 
+import com.google.common.base.Strings;
 import com.google.common.collect.Maps;
 import com.sup.cms.bean.po.LoanRepayInfoGetListBean;
 import com.sup.cms.bean.po.LoanUnRepayInfoGetListBean;
@@ -7,6 +8,7 @@ import com.sup.cms.bean.vo.LoanRepayInfoGetListParams;
 import com.sup.cms.bean.vo.LoanUnRepayInfoGetListParams;
 import com.sup.cms.mapper.CrazyJoinMapper;
 import com.sup.cms.util.ResponseUtil;
+import com.sup.common.loan.ApplyStatusEnum;
 import com.sup.common.param.ReductionParam;
 import com.sup.common.util.Result;
 import lombok.extern.slf4j.Slf4j;
@@ -41,6 +43,38 @@ public class LoanController {
         if (params.getRepayNeedConfirm() != null && params.getRepayNeedConfirm().equals(1)) {
             sb.append(" and g.status=0");
         }
+        if (params.getShouldRepayDateStart() != null) {
+            sb.append(" and f.repay_end_date>=" + params.getShouldRepayDateStart());
+        }
+        if (params.getShouldRepayDateEnd() != null) {
+            sb.append(" and f.repay_start_date<=" + params.getShouldRepayDateEnd());
+        }
+        if (params.getActualRepayDateStart() != null) {
+            sb.append(" and f.repay_time>=" + params.getActualRepayDateStart());
+        }
+        if (params.getActualRepayDateEnd() != null) {
+            sb.append(" and f.repay_time<=" + params.getActualRepayDateEnd());
+        }
+
+        if (params.getProductId() != null) {
+            sb.append(" and a.product_id=" + params.getProductId());
+        }
+        if (!Strings.isNullOrEmpty(params.getCidNo())) {
+            sb.append(" and c.cid_no=" + params.getCidNo());
+        }
+        if (params.getApplyId() != null) {
+            sb.append(" and a.id=" + params.getApplyId());
+        }
+        if (!Strings.isNullOrEmpty(params.getMobile())) {
+            sb.append(" and d.mobile=" + params.getMobile());
+        }
+        if (params.getStatus() != null) {
+            if (params.getStatus() == 0) {  // 未核销
+                sb.append(" and a.status!=" + ApplyStatusEnum.APPLY_WRITE_OFF.getCode());
+            } else {
+                sb.append(" and a.status=" + ApplyStatusEnum.APPLY_WRITE_OFF.getCode());
+            }
+        }
         Integer offset = (params.getPage() - 1) * params.getPageSize();
         Integer rows = params.getPageSize();
         List<LoanRepayInfoGetListBean> l = crazyJoinMapper.loanRepayInfoGetList(sb.toString(), offset, rows);
@@ -58,6 +92,26 @@ public class LoanController {
     @PostMapping("/unRepayInfo/getList")
     public String unRepayInfoGetList(@RequestBody @Valid LoanUnRepayInfoGetListParams params) {
         StringBuilder sb = new StringBuilder();
+        if (params.getShouldRepayDateStart() != null) {
+            sb.append(" and f.repay_end_date>=" + params.getShouldRepayDateStart());
+        }
+        if (params.getShouldRepayDateEnd() != null) {
+            sb.append(" and f.repay_start_date<=" + params.getShouldRepayDateEnd());
+        }
+
+        if (params.getProductId() != null) {
+            sb.append(" and a.product_id=" + params.getProductId());
+        }
+        if (!Strings.isNullOrEmpty(params.getCidNo())) {
+            sb.append(" and c.cid_no=" + params.getCidNo());
+        }
+        if (params.getApplyId() != null) {
+            sb.append(" and a.id=" + params.getApplyId());
+        }
+        if (!Strings.isNullOrEmpty(params.getMobile())) {
+            sb.append(" and d.mobile=" + params.getMobile());
+        }
+
         Integer offset = (params.getPage() - 1) * params.getPageSize();
         Integer rows = params.getPageSize();
         List<LoanUnRepayInfoGetListBean> l = crazyJoinMapper.loanUnRepayInfoGetList(sb.toString(), offset, rows);
