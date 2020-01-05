6bef13a87f958d91bc34c2ffbb28c8a72d23e19e
gongshuai <shuaigong1@yirendai.com>
2020-01-05 22:23:06 +0800

    add   black list  inner and refactor  credit level

---
+++ common/src/main/java/com/sup/common/param/UpdateCreditLevelParam.java
@@ -0,0 +1,9 @@
+package com.sup.common.param;
+
+/**
+ * gongshuai
+ * <p>
+ * 2020/1/5
+ */
+public class UpdateCreditLevelParam {
+}
---
+++ core/src/main/java/com/sup/core/service/RuleConfigService.java
@@ -0,0 +1,58 @@
+package com.sup.common.service;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.sup.common.bean.CreditLevelRuleBean;
+import com.sup.common.bean.TbApplyInfoBean;
+import com.sup.common.bean.TbUserRegistInfoBean;
+import com.sup.common.loan.ApplyStatusEnum;
+import lombok.extern.log4j.Log4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * gongshuai
+ * <p>
+ * 2020/1/5
+ */
+@Log4j
+@Service
+public class RuleConfigService {
+
+    @Autowired
+    private UserRegisterInfoMapper userRegisterInfoMapper;
+
+    @Autowired
+    private CreditLevelRulesMapper creditLevelRulesMapper;
+    @Autowired
+    private ApplyInfoMapper applyInfoMapper;
+
+    public void updateCreditLevelForUser(String user_id) {
+
+
+        List<CreditLevelRuleBean> creditLevelRuleBeans = this.creditLevelRulesMapper.selectList(new QueryWrapper<CreditLevelRuleBean>().orderByDesc("level"));
+        List<TbApplyInfoBean> applyInfoBeanList = this.applyInfoMapper.selectList(new QueryWrapper<TbApplyInfoBean>().eq("status", ApplyStatusEnum.APPLY_REPAY_ALL).eq("user_id", Integer.parseInt(user_id)));  //结清状态的申请单
+        int reloan_times = applyInfoBeanList != null ? applyInfoBeanList.size() : 0;
+        this.updateCreditLevelForUser(user_id, reloan_times, creditLevelRuleBeans);
+    }
+
+    public void updateCreditLevelForUser(String user_id, Integer reloan_times, List<CreditLevelRuleBean> creditLevelRuleBeans) {
+
+        OverdueInfoBean overdueInfoBean = OverdueUtils.getMaxOverdueDays(user_id, this.repayPlanMapper);
+        for (CreditLevelRuleBean creditLevelRuleBean : creditLevelRuleBeans) {
+            if (reloan_times >= creditLevelRuleBean.getReloan_times() && overdueInfoBean.getMax_days() <= creditLevelRuleBean.getMax_overdue_days()) {
+                TbUserRegistInfoBean userRegistInfoBean = this.userRegisterInfoMapper.selectById(Integer.parseInt(user_id));
+                if (userRegistInfoBean != null) {
+                    userRegistInfoBean.setCredit_level(creditLevelRuleBean.getLevel());
+                    this.userRegisterInfoMapper.updateById(userRegistInfoBean);
+                } else {
+                    log.error("Failed to update Credit level user_id:" + user_id);
+                }
+                break;
+            }
+
+        }
+
+    }
+}
