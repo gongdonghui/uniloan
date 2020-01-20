67acd4ab221a8c125c8ebb6b356aa2e75e4284ea
ghost <guanfeng2007@qq.com>
2020-01-20 16:28:42 +0800

    update cms(add operation report new API)

---
+++ cms/src/main/java/com/sup/cms/bean/po/LoanStatBean.java
@@ -0,0 +1,35 @@
+package com.sup.cms.bean.po;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * Project:uniloan
+ * Class:  LoanStatBean
+ * <p>
+ * Author: guanfeng
+ * Create: 2019-10-12
+ */
+
+@Data
+public class LoanStatBean {
+
+    private Date    dt;
+    private Integer loanNum;                // 放款数
+    private Long    principal;              // 放款本金
+    private Long    contractAmt;            // 合同金额
+    private Long    normalRepayAmt;         // 正常还款金额
+    private Integer normalRepayNum;         // 正常还款次数
+    private Integer firstOverdueNum;        // 首逾笔数
+    private Long    firstOverdueAmt;        // 首逾金额
+    private Long    overduedContractAmt;    // 逾期已还合同金额
+    private Long    overduedPenaltyInterest;// 逾期已还罚息
+    private Long    overduingContractAmt;   // 逾期中合同金额
+    private Long    repayTotal;             // 总回款
+    private Float   foRate;                 // 首逾率
+    private Integer overdueNum;             // 当前逾期笔数
+    private Float   overdueRate;            // 当前逾期率
+    private Float   principalRate;          // 本金回收率
+
+}
--- common/src/main/java/com/sup/common/param/OperationReportParam.java
+++ common/src/main/java/com/sup/common/param/OperationReportParam.java
@@ -3,6 +3,7 @@ package com.sup.common.param;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
+import javax.validation.constraints.Min;
 import java.util.Date;
 
 /**
@@ -19,5 +20,12 @@ public class OperationReportParam {
     private Date end_date;
 
     private Integer channel_id;
+    private Integer product_id;
     private Integer type;   //0  渠道日报   2   运营日报   3     审批日报，  4  催收日报
+
+    @Min(0)
+    private Integer page;
+
+    @Min(0)
+    private Integer pageSize;
 }
