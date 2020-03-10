d8d6504c71cb8e9079aac7c862ff5910c4e9f636
ghost <guanfeng2007@qq.com>
2020-03-10 15:36:49 +0800

    合并20200204_kalapa 分支； 渠道管理、运营日志增加appid

--- kalapa/src/main/java/com/sup/kalapa/KalapaApplication.java
+++ kalapa/src/main/java/com/sup/kalapa/KalapaApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableEurekaClient
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 @EnableFeignClients(basePackages = {"com.sup.common.service"})
 public class KalapaApplication {
 
