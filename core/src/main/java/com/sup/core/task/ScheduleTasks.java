9d420178749760de375deac69e66834b71e4251c
gongshuai <shuaigong1@yirendai.com>
2020-03-15 22:05:15 +0800

    fix  bug

--- core/src/main/java/com/sup/core/task/ScheduleTasks.java
+++ core/src/main/java/com/sup/core/task/ScheduleTasks.java
@@ -499,7 +499,7 @@ public class ScheduleTasks {
     /**
      * 每天更新资产等级，
      */
-    @Scheduled(cron = "0 03 21 * * ?")
+    @Scheduled(cron = "0 09 21 * * ?")
     public void updateAssertLevel() {
         log.info("AssetLevel update...");
         List<TbApplyInfoBean> applyInfoBeanList = this.applyInfoMapper.selectList(new QueryWrapper<TbApplyInfoBean>()
