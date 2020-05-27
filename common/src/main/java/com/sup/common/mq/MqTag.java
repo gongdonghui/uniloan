6b388698344a97048d60315e3fdc17fde16d6c6e
zhouxidong <xidongzhou1@creditease.cn>
2020-05-28 00:11:01 +0800

    加入催收消息

--- common/src/main/java/com/sup/common/mq/MqTag.java
+++ common/src/main/java/com/sup/common/mq/MqTag.java
@@ -8,7 +8,10 @@ public class MqTag {
   public static final String ISSUE_VERIFY_CODE = "issue_verify_code";
   public static final String ONEDAY_OVERDUE_NOTIFY = "oneday_overdue_notify";
   public static final String ONEDAY_TO_REPAY_NOTIFY = "oneday_to_repay_notify";
+
   public static final String DAY_TO_REPAY_NOTIFY = "day_to_repay_notify";
+  public static final String ONE_DAY_OVERDUE_NOTIFY = "one_day_overdue_notify";
+  public static final String TWO_DAY_OVERDUE_NOTIFY = "two_days_overdue_notify";
 
   public static final String APPLY_STATUS_CHANGE = "apply_status_change";
   public static final String REPAY_SUCC_NOTIFY = "repay_succ_notify";
